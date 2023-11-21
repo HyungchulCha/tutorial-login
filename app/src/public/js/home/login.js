5ca06c7b86d0eff2ad686d1d6cc89eaebc4ba830
ckgudcjf <ckgudcjf@gmail.com>
2023-11-21 11:41:48 +0900

    ing

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,15 @@
-console.log('aaa')
+const userid = document.querySelector("#userid");
+const userpassword = document.querySelector("#userpassword");
+const loginbutton = document.querySelector("button");
+
+console.log(userid);
+
+loginbutton.addEventListener("click", fnLogin);
+
+function fnLogin() {
+  const req = {
+    userid: userid.value,
+    userpassword: userpassword.value,
+  };
+  console.log(req);
+};
