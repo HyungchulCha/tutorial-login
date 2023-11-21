7bc1c502ef594cbd51a0007ea47761b7713b5418
ckgudcjf <ckgudcjf@gmail.com>
2023-11-21 17:19:39 +0900

    ing

--- app/app.js
+++ app/app.js
@@ -21,7 +21,6 @@ app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`))
 
-
 app.use("/", home);
 
 module.exports = app
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,5 +11,12 @@ function fnLogin() {
     userid: userid.value,
     userpassword: userpassword.value,
   };
-  console.log(req);
+  console.log(req, JSON.stringify(req))
+  fetch('/login', {
+    method: 'POST',
+    headers: {
+        "Content-Type": 'application/json'
+    },
+    body: JSON.stringify(req)
+  })
 };
