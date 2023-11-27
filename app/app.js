1e161dc4e312615bc34d96e8a42a8e5d538b32b8
ckgudcjf <ckgudcjf@gmail.com>
2023-11-27 14:09:35 +0900

    ing

--- app/app.js
+++ app/app.js
@@ -14,20 +14,19 @@
 
 const dotenv = require("dotenv");
 dotenv.config();
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const morgan = require("morgan");
+const accessLogStream = require("./src/config/log");
 const home = require("./src/routes/home");
-
-
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(`${__dirname}/src/public`));
+app.use(morgan("dev", { stream: accessLogStream }));
 
 app.use("/", home);
 
